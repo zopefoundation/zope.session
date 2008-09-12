8fc43f8a3787b3a7dff79fd956a813192129fc11
Jim Fulton <jim@zope.com>
2008-09-12 16:26:47 +0000

    Updated version #

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(name='zope.session',
-    version = '3.5.3dev',
+    version = '3.6.1dev',
     author='Zope Corporation and Contributors',
     author_email='zope3-dev@zope.org',
     description='Zope 3 Session Support',
