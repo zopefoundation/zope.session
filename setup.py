327a6d8cf54a42e2aa417e0275d747b28f98dab4
Hanno Schlichting <hanno@hannosch.eu>
2009-04-20 20:31:28 +0000

    Update version after release

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(name='zope.session',
-    version='3.9.1',
+    version='3.9.2dev',
     author='Zope Corporation and Contributors',
     author_email='zope-dev@zope.org',
     description='Client identification and sessions for Zope',
