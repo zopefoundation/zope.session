22047ef4409563aa22add048dee6a75a894385be
Patrick Strawderman <patrick@kilink.net>
2009-11-23 15:21:04 +0000

    Preparing release 3.9.2

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(name='zope.session',
-    version='3.9.2dev',
+    version='3.9.3',
     author='Zope Corporation and Contributors',
     author_email='zope-dev@zope.org',
     description='Client identification and sessions for Zope',
