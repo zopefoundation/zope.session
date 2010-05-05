ba1c35e200e8afcdc8b805ab3b84f2550b510fb2
Tres Seaver <tseaver@palladion.com>
2010-05-05 19:27:10 +0000

    Conform to repository policy.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 ##############################################################################
 #
-# Copyright (c) 2006 Zope Corporation and Contributors.
+# Copyright (c) 2006 Zope Foundation and Contributors.
 # All Rights Reserved.
 #
 # This software is subject to the provisions of the Zope Public License,
@@ -30,7 +30,7 @@ def read(*rnames):
 
 setup(name='zope.session',
     version='3.9.3dev',
-    author='Zope Corporation and Contributors',
+    author='Zope Foundation and Contributors',
     author_email='zope-dev@zope.org',
     description='Client identification and sessions for Zope',
     long_description=(
