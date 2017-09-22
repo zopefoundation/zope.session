247c7ac2944f139fc675c9f6663ec3762f082483
Jason Madden <jamadden@gmail.com>
2017-09-22 12:40:09 -0500

    Preparing release 4.2.0

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ TESTS_REQUIRE = [
 ]
 
 setup(name='zope.session',
-      version='4.2.0.dev0',
+      version='4.2.0',
       author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.org',
       description='Client identification and sessions for Zope',
