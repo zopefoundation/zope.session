b5f99d4df0824923e123c9887babba0d4ae76769
Jason Madden <jamadden@gmail.com>
2017-09-22 12:40:45 -0500

    Back to development: 4.2.1

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ TESTS_REQUIRE = [
 ]
 
 setup(name='zope.session',
-      version='4.2.0',
+      version='4.2.1.dev0',
       author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.org',
       description='Client identification and sessions for Zope',
