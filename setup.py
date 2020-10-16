fa2ae39428fab60538f4fb7b43c531a71c180937
Marius Gedminas <marius@gedmin.as>
2020-10-16 17:50:03 +0300

    Back to development: 4.4.1

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ TESTS_REQUIRE = [
 ]
 
 setup(name='zope.session',
-      version='4.4.0',
+      version='4.4.1.dev0',
       author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.org',
       description='Client identification and sessions for Zope',
