11d73278b3a0ea70620322655e26a4359bea0149
Michael Howitz <icemac@gmx.net>
2025-09-12 09:42:30 +0200

    Preparing release 6.0

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ TESTS_REQUIRE = [
 ]
 
 setup(name='zope.session',
-      version='6.0.dev0',
+      version='6.0',
       author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.dev',
       description='Client identification and sessions for Zope',
