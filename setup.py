34c6fe52c2c59d5085ff010554535e5aa5682882
Michael Howitz <icemac@gmx.net>
2026-02-02 10:43:05 +0100

    config with pure python template 3289c8f3 (#25)
    * Drop support for Python 3.9.
    
    * Add support for Python 3.14.
    
    * Configuring for pure-python
    
    * Update Python version support.
    
    * Move package metadata from setup.py to pyproject.toml.
    
    * Move package metadata from setup.py to pyproject.toml.
    
    * Update documentation configuration to use importlib.metadata and dynamic copyright year

--- setup.py
+++ setup.py
@@ -18,73 +18,8 @@
 ##############################################################################
 """Setup for zope.session package
 """
-import os
-
 from setuptools import setup
 
 
-def read(*rnames):
-    with open(os.path.join(os.path.dirname(__file__), *rnames)) as f:
-        return f.read()
-
-
-TESTS_REQUIRE = [
-    'zope.configuration',
-    'zope.testing',
-    'zope.testrunner >= 6.4',
-]
-
-setup(name='zope.session',
-      version='6.1.dev0',
-      author='Zope Foundation and Contributors',
-      author_email='zope-dev@zope.dev',
-      description='Client identification and sessions for Zope',
-      long_description=(
-          read('README.rst')
-          + '\n\n' +
-          read('CHANGES.rst')
-      ),
-      license='ZPL-2.1',
-      keywords="zope3 session",
-      classifiers=[
-          'Development Status :: 5 - Production/Stable',
-          'Environment :: Web Environment',
-          'Intended Audience :: Developers',
-          'License :: OSI Approved :: Zope Public License',
-          'Programming Language :: Python',
-          'Programming Language :: Python :: 3',
-          'Programming Language :: Python :: 3.9',
-          'Programming Language :: Python :: 3.10',
-          'Programming Language :: Python :: 3.11',
-          'Programming Language :: Python :: 3.12',
-          'Programming Language :: Python :: 3.13',
-          'Programming Language :: Python :: Implementation :: CPython',
-          'Programming Language :: Python :: Implementation :: PyPy',
-          'Natural Language :: English',
-          'Operating System :: OS Independent',
-          'Topic :: Internet :: WWW/HTTP',
-          'Framework :: Zope :: 3',
-      ],
-      url='https://github.com/zopefoundation/zope.session',
-      python_requires='>=3.9',
-      install_requires=[
-          'setuptools',
-          'ZODB >= 4.2.0.b1',
-          'zope.component',
-          'zope.i18nmessageid >= 4.0.3',
-          'zope.interface',
-          'zope.location',
-          'zope.publisher >= 4.1.0',
-          'zope.minmax',
-          'zope.traversing',
-      ],
-      extras_require={
-          'test': TESTS_REQUIRE,
-          'docs': [
-              'Sphinx',
-              'repoze.sphinx.autointerface',
-          ],
-      },
-      include_package_data=True,
-      zip_safe=False,
-      )
+# See pyproject.toml for package metadata
+setup()
